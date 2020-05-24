6e815cf681d3a2affec4495e09d31a34590797da
Enes Karaosman <enes.karaosman@etiya.com>
2020-05-24 17:57:21 +0300

    [ADD] Mock image resources added.

--- Package.swift
+++ Package.swift
@@ -23,6 +23,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftyChat",
-            dependencies: [])
+            dependencies: []
+//            sources: [
+//                .init("Mock/Resources/*")
+//            ]
+        )
     ]
 )
