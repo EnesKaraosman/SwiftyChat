b1edf776aac536e7f57068b8c2c1f75f356beaf6
Enes Karaosman <enes.karaosman@etiya.com>
2020-06-03 16:40:15 +0300

    [FIX] Access initializer fixed on TextTappedCallback

--- Sources/SwiftyChat/Common/Other/TextTappedCallback.swift
+++ Sources/SwiftyChat/Common/Other/TextTappedCallback.swift
@@ -8,11 +8,31 @@
 import Foundation
 
 public struct TextTappedCallback {
-    public var didSelectAddress: (_ addressComponents: [String: String]) -> Void = { _ in }
-    public var didSelectDate: (_ date: Date) -> Void = { _ in }
-    public var didSelectPhoneNumber: (_ phoneNumber: String) -> Void = { _ in }
-    public var didSelectURL: (_ url: URL) -> Void = { _ in }
-    public var didSelectTransitInformation: (_ transitInformation: [String: String]) -> Void = { _ in }
-    public var didSelectMention: (_ mention: String) -> Void = { _ in }
-    public var didSelectHashtag: (_ hashtag: String) -> Void = { _ in }
+    
+    public var didSelectAddress: (_ addressComponents: [String: String]) -> Void
+    public var didSelectDate: (_ date: Date) -> Void
+    public var didSelectPhoneNumber: (_ phoneNumber: String) -> Void
+    public var didSelectURL: (_ url: URL) -> Void
+    public var didSelectTransitInformation: (_ transitInformation: [String: String]) -> Void
+    public var didSelectMention: (_ mention: String) -> Void
+    public var didSelectHashtag: (_ hashtag: String) -> Void
+    
+    public init(
+        didSelectAddress: @escaping (_ addressComponents: [String: String]) -> Void = { _ in },
+        didSelectDate: @escaping (_ date: Date) -> Void = { _ in },
+        didSelectPhoneNumber: @escaping (_ phoneNumber: String) -> Void = { _ in },
+        didSelectURL: @escaping (_ url: URL) -> Void = { _ in },
+        didSelectTransitInformation: @escaping (_ transitInformation: [String: String]) -> Void = { _ in },
+        didSelectMention: @escaping (_ mention: String) -> Void = { _ in },
+        didSelectHashtag: @escaping (_ hashtag: String) -> Void = { _ in }
+    ) {
+        self.didSelectAddress = didSelectAddress
+        self.didSelectDate = didSelectDate
+        self.didSelectPhoneNumber = didSelectPhoneNumber
+        self.didSelectURL = didSelectURL
+        self.didSelectTransitInformation = didSelectTransitInformation
+        self.didSelectMention = didSelectMention
+        self.didSelectHashtag = didSelectHashtag
+    }
+    
 }
