526001fd16b625e705fb108ab9ad29199a16567e
Enes Karaosman <enes.karaosman@etiya.com>
2020-08-27 11:53:49 +0300

    [UPG] KingFisher image downloading spm version increased.

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         
         // Image downloading library
-        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "5.14.1"),
+        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "5.15.0"),
         
         // Introspecting underlying UIKit components
 //        .package(url: "https://github.com/siteline/SwiftUI-Introspect.git", from: "0.1.0")
