2ce729dc17a2973203a2a65140425625b29d7a4b
Enes Karaosman <enes.karaosman@etiya.com>
2020-10-20 18:09:41 +0300

    Example project added

---
+++ SwiftyChatExample/SwiftyChatExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftyChatExample
+//
+//  Created by Enes Karaosman on 20.10.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
