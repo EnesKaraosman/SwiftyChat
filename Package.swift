637a7f5bffa731389a4e10088f44d1fff409254f
Enes Karaosman <eneskaraosman53@gmail.com>
2022-03-16 19:57:56 +0300

    fix: Fix build issue on CI/CD with adding macOS to the supported platform for version 10.15+

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "SwiftyChat",
     platforms: [
-        .iOS(.v14)
+        .iOS(.v14),
+        .macOS(.v10_15)
     ],
     products: [
         .library(
