5967373fb5c47bf87da40a096242a32e1f9fe1b8
Amiel Reyes <kaareyes.developer@gmail.com>
2023-02-15 17:29:25 +0800

    disable email

--- Sources/SwiftyChat/MessageCells/TextCell.swift
+++ Sources/SwiftyChat/MessageCells/TextCell.swift
@@ -108,16 +108,17 @@ internal struct TextCell<Message: ChatMessage>: View {
     }
     
     @ViewBuilder public var body: some View {
-        if text.containsHtml() ||
-            AttributeDetective(
-                text: text,
-                enabledDetectors: enabledDetectors
-            ).doesContain()
-        {
-            attributedText
-        } else {
-            defaultText
-        }
+//        if text.containsHtml() ||
+//            AttributeDetective(
+//                text: text,
+//                enabledDetectors: enabledDetectors
+//            ).doesContain()
+//        {
+//            attributedText
+//        } else {
+//            defaultText
+//        }
+        defaultText
     }
     
 }
