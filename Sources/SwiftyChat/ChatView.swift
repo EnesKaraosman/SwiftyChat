0690435d2fae94dbca04c00495394324424d6759
Amiel Reyes <kaareyes.developer@gmail.com>
2023-05-29 23:51:34 +0800

    update dismiss keyboard

--- Sources/SwiftyChat/ChatView.swift
+++ Sources/SwiftyChat/ChatView.swift
@@ -46,14 +46,7 @@ public struct ChatView<Message: ChatMessage, User: ChatUser>: View {
     
     
     private func dismissKeyboard(){
-        let keyWindow = UIApplication.shared.connectedScenes
-                .filter({$0.activationState == .foregroundActive})
-                .map({$0 as? UIWindowScene})
-                .compactMap({$0})
-                .first?.windows
-                .filter({$0.isKeyWindow}).first
-        keyWindow?.endEditing(true)
-
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
     public var body: some View {
         GeometryReader { geometry in
