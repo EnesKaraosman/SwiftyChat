dab5afab830ca925889534c3e8d7ad307bcc390d
Enes Karaosman <eneskaraosman53@gmail.com>
2024-04-05 23:44:19 +0300

    refactor: drop macOS 11 support

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,7 @@ let package = Package(
     name: "SwiftyChat",
     platforms: [
         .iOS(.v14),
-        .macOS(.v11)
+        .macOS(.v12)
     ],
     products: [
         .library(
