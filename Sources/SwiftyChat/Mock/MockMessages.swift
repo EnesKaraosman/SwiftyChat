18eff13a4f2474fca1062dda41087ed5d764bee7
Enes Karaosman <eneskaraosman53@gmail.com>
2024-04-09 14:53:09 +0300

    Update MockMessages.swift

--- Sources/SwiftyChat/Mock/MockMessages.swift
+++ Sources/SwiftyChat/Mock/MockMessages.swift
@@ -9,15 +9,6 @@
 import UIKit
 import Foundation
 
-internal extension UIColor {
-    func image(_ size: CGSize = CGSize(width: 1, height: 1)) -> UIImage {
-        return UIGraphicsImageRenderer(size: size).image { rendererContext in
-            self.setFill()
-            rendererContext.fill(CGRect(origin: .zero, size: size))
-        }
-    }
-}
-
 public struct MockMessages {
     
     public enum Kind {
