7116c44142c01b5ea57bb93b119d165ef3a96047
Enes Karaosman <eneskaraosman53@gmail.com>
2024-04-12 21:57:53 +0300

    fix: improve advanced example view appear time

--- Example/Example/AdvancedExampleView.swift
+++ Example/Example/AdvancedExampleView.swift
@@ -9,19 +9,18 @@ import SwiftUI
 import SwiftyChat
 
 struct AdvancedExampleView: View {
-    
+
     @State var messages: [MockMessages.ChatMessageItem] = []
     @State private var scrollToBottom = false
-    
+
     @State private var message = ""
-    
+
     var body: some View {
         chatView
     }
-    
+
     private var chatView: some View {
         ChatView<MockMessages.ChatMessageItem, MockMessages.ChatUserItem>(messages: $messages) {
-
             BasicInputView(
                 message: $message,
                 placeholder: "Type something",
@@ -33,7 +32,6 @@ struct AdvancedExampleView: View {
             )
             .background(Color.primary.colorInvert())
             .embedInAnyView()
-            
         }
         // ▼ Optional, Implement to register a custom cell for Messagekind.custom
         .registerCustomCell(customCell: { anyParam in AnyView(CustomExampleChatCell(anyParam: anyParam))})
@@ -86,41 +84,32 @@ struct AdvancedExampleView: View {
         .navigationBarTitle("Advanced")
         #endif
         .listStyle(PlainListStyle())
-        .onAppear {
-   
-            if let portraitUrl = URL(string: "https://picsum.photos/200/300") {
+        .task {
+            if let portraitUrl = URL(string: "https://picsum.photos/id/\(Int.random(in: 1...100))/400/600") {
                 self.messages.append(.init(user: MockMessages.chatbot, messageKind: .image(.remote(portraitUrl))))
             }
 
-            if let landscapeUrl = URL(string:"https://picsum.photos/400/200") {
-                self.messages.append(.init(user: MockMessages.chatbot, messageKind: .image(.remote(landscapeUrl))))
-            }
-
-            
             self.messages.append(
                 .init(
                     user: MockMessages.chatbot,
                     messageKind: .text("https://github.com/EnesKaraosman/SwiftyChat and here is his phone +90 537 844 11-41, & mail: eneskaraosman53@gmail.com Today is 27 May 2020")
                 )
             )
-            
-            self.messages.append(contentsOf: MockMessages.generatedMessages(count: 20))
+
+            self.messages.append(contentsOf: MockMessages.generatedMessages(count: 53))
         }
     }
 }
 
 struct CustomExampleChatCell: View {
     var anyParam: Any
-    
+
     var body: some View {
-        VStack{
-            Text((anyParam as? String) ?? "Not a String")
-                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
-                .padding(5)
-                                
-        }
-        .background(Color.green)
-        .cornerRadius(25)
+        Text((anyParam as? String) ?? "Not a String")
+            .frame(maxWidth: .infinity)
+            .padding()
+            .background(Color.green)
+            .cornerRadius(24)
     }
 }
 
